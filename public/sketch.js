5d2e241925ba429b8c0266cb3c8240278dc36d47
bkemp1004 <56891577+bkemp1004@users.noreply.github.com>
2020-01-04 16:06:37 -0600

    Added Project Files

---
+++ public/sketch.js
@@ -0,0 +1,36 @@
+var socket;
+var data;
+
+function setup(){
+    createCanvas(windowWidth-10,windowHeight-10);
+    background(51);
+    socket = io.connect("http://167.172.124.109:3000/");
+    socket.on('mouse', newDraw);    
+}
+
+function newDraw(mouse){
+    fill(200);
+    noStroke();
+    ellipse(mouse.x,mouse.y,30,30);
+    stroke(200);
+    strokeWeight(30);
+    line(mouse.px,mouse.py,mouse.x,mouse.y);
+}
+
+function mouseDragged(){
+    fill(255);
+    noStroke();
+    ellipse(mouseX,mouseY,30,30);
+    stroke(255);
+    strokeWeight(30);
+    line(pmouseX,pmouseY,mouseX,mouseY);
+    if (mouseIsPressed){
+        mouse = {
+            x : mouseX,
+            y : mouseY,
+            px : pmouseX,
+            py : pmouseY
+        }
+        socket.emit('mouse', mouse);
+    }
+}
