df3f77a5f372ff0e675fc1e52633eda15fc21c67
bkemp1004 <56891577+bkemp1004@users.noreply.github.com>
2020-01-04 19:11:42 -0600

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -30,20 +30,22 @@ function mouseDragged(){
     for (i in sliders){
         clientId[i] = sliders[i].value();
     }
-    fill(clientId[0],clientId[1],clientId[2]);
-    noStroke();
-    ellipse(mouseX,mouseY,30,30);
-    stroke(clientId[0],clientId[1],clientId[2]);
-    strokeWeight(30);
-    line(pmouseX,pmouseY,mouseX,mouseY);
-    if (mouseIsPressed){
-        mouse = {
-            x : mouseX,
-            y : mouseY,
-            px : pmouseX,
-            py : pmouseY,
-            colorId: clientId
+    if(mouseY <= height){
+        fill(clientId[0],clientId[1],clientId[2]);
+        noStroke();
+        ellipse(mouseX,mouseY,30,30);
+        stroke(clientId[0],clientId[1],clientId[2]);
+        strokeWeight(30);
+        line(pmouseX,pmouseY,mouseX,mouseY);
+        if (mouseIsPressed){
+            mouse = {
+                x : mouseX,
+                y : mouseY,
+                px : pmouseX,
+                py : pmouseY,
+                colorId: clientId
+            }
+            socket.emit('mouse', mouse);
         }
-        socket.emit('mouse', mouse);
     }
 }
