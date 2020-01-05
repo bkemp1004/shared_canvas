ba8a6dc45e1883941f7da7f9804e94b5e626bea3
bkemp1004 <56891577+bkemp1004@users.noreply.github.com>
2020-01-04 18:01:04 -0600

    Added multicolor

--- public/sketch.js
+++ public/sketch.js
@@ -3,16 +3,16 @@ var socket;
 function setup(){
     createCanvas(windowWidth-20,windowHeight-20);
     background(51);
-    clientId = random(175);
+    clientId = [random(175),random(175),random(175)];
     socket = io.connect("http://167.172.124.109:3000/");
     socket.on('mouse', newDraw);    
 }
 
 function newDraw(mouse){
-    fill(mouse.colorId);
+    fill(mouse.colorId[0],mouse.colorId[1],mouse.colorId[2]);
     noStroke();
     ellipse(mouse.x,mouse.y,30,30);
-    stroke(mouse.colorId);
+    stroke(mouse.colorId[0],mouse.colorId[1],mouse.colorId[2]);
     strokeWeight(30);
     line(mouse.px,mouse.py,mouse.x,mouse.y);
 }
